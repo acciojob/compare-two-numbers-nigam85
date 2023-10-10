514882d491be6e879d41c887bf4ccee331aa3c80
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-10 14:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+// Prompt the user for the first number and convert it to a number
+const num1 = parseFloat(prompt('Enter the first number:'));
+
+// Prompt the user for the second number and convert it to a number
+const num2 = parseFloat(prompt('Enter the second number:'));
+
+// Check if the two numbers are equal and store the result in a variable
+const isEqual = num1 === num2;
+
+// Display the result in an alert box
+alert(`Are the numbers equal? ${isEqual}`);
